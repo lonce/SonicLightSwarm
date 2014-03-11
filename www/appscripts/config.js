7189a5e9e24c8d3076e754e2e3f620520626c7c0
lonce <cnmwll@nus.edu.sg>
2014-03-11 19:54:51 +0800

    initial version, forked from AntScore

---
+++ www/appscripts/config.js
@@ -0,0 +1,15 @@
+define(
+	function () {
+		exports = {};
+		exports.webketAudioEnabled=true;
+
+		if (!window.webkitAudioContext) {
+			alert("Web Audio API is not supported. You can join a score room interactively as a controller, but you won't hear your sounds unless you are within earshot of a webkitAudio enabled machine in the same score room.");
+			exports.webketAudioEnabled=false;
+		}
+
+		exports.touchMarginOfError = 3; //px, used for "selecting" items on the score
+		exports.minSndDuration=60; // must be longer than frame duration so start and stop and not sent to the synthesizer at the same time. 
+		return exports;
+});
+
